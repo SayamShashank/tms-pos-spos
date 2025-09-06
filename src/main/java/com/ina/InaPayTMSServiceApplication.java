f88d737cec55f54d8ffd4b95e0ebd5189a9b89cf
charanrayalla <venky@inapayments.com>
2025-09-06 17:00:14 +0530

    redis server changes

--- src/main/java/com/ina/InaPayTMSServiceApplication.java
+++ src/main/java/com/ina/InaPayTMSServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories({"com.ina.common.dao", "com.ina.transaction.repository","com.ina.common.crypto.repository","com.ina.dao"})
 @SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 @EnableScheduling
+@EnableCaching
 public class InaPayTMSServiceApplication {
     public static void main(String[] args)  {
 
